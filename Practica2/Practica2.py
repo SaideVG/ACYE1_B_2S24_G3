9382160aa7ff8c5bf2601bac8604dd468964a027
Oscar Padilla <99352629+Ospadi@users.noreply.github.com>
2024-08-19 21:31:23 -0600

    se agregaron los pines de la raspberry

---
+++ Practica2/Practica2.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+from time import sleep, time
+import spidev
+import drivers
+
+# Inicialización de la LCD usando la librería drivers
+lcd = drivers.Lcd()
+
+# Variables para el funcionamiento del controlador de la temperatura
+spi = spidev.SpiDev()
+spi.open(0, 0)
+
+# Puerto de la temperatura CH0
+channel_temp = 0
+
+# Función para leer el puerto SPI
+def ReadChannel(channel):
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
+
+# Función para convertir el voltaje recibido a temperatura
+def ConvertTemp(data, places):
+    temp = ((data * 330) / float(1023))
+    temp = round(temp, places)
+    return temp
+
+# Configuración de los pines usando BCM
+GPIO.setmode(GPIO.BCM)
+
+# Pines de luz
+boton_pin = 12  # GPIO 12
+led_pin = 13    # GPIO 13
+
+# Pines para la contraseña
+BTN_1 = 4   # GPIO 4
+BTN_2 = 17  # GPIO 17
+BTN_3 = 27  # GPIO 27
+BTN_4 = 22  # GPIO 22
+
+# Pines para el funcionamiento del motor
+MOTOR_1 = 26  # GPIO 26
+MOTOR_2 = 19  # GPIO 19
+
+# Pines para el funcionamiento del sensor de flama y buzzer
+flame_Sensor = 5    # GPIO 5
+btn_on_off_buzzer = 6  # GPIO 6
+buzzer = 21   # GPIO 21
